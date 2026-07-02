fba3f2901273eeef4fe9128316205a23fd76757f
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-07-02 20:33:48 +0530

    feat : calculate hash value for a tuple of integers

--- problem17.py
+++ problem17.py
@@ -1,1 +1,5 @@
-problem17.py
+# Create a tuple from a sequence of $n$ input integers and print the resulting hash value using Python’s built-in hash() function.
+if __name__ == '__main__':
+    n = int(raw_input())
+    integer_list = map(int, input().split())
+    print(hash(tuple(integer_list)))
