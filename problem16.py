d2bba99739a53413db4c5bb4dd1f2d4a8bd28c42
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-07-02 20:24:57 +0530

    feat : implement dynamic command processing for list operations using getattr

---
+++ problem16.py
@@ -0,0 +1,11 @@
+#Implement a command-driven interface to perform dynamic list operations, including insertion, deletion, sorting, and reversal, based on sequential input commands.
+if __name__ == '__main__':
+    N = int(input())
+    my_list = []
+    for _ in range(N):
+        cmd, *args = input().split()
+        args = list(map(int, args))
+        if cmd == "print":
+            print(my_list)
+        else:
+            getattr(my_list, cmd)(*args)
