320e1fee340fe0e2d1e8a0153fc7017bb8472959
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-07-03 22:33:32 +0530

    string case swapping

---
+++ problem22.py
@@ -0,0 +1,7 @@
+#Given a string, return a new string where all lowercase letters are converted to uppercase letters and vice versa.
+def swap_case(s):
+    return s.swapcase()
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
