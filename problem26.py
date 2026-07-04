8c782cbfc57a83c41850917c19fa92376073b74f
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-07-04 22:50:14 +0530

    feat(hackerrank): implement overlapping substring counter using sliding window generator

--- problem26.py
+++ problem26.py
@@ -1,1 +1,8 @@
-problem26.py
+# HackerRank: Replace a character at a specific index in an immutable string using optimal slicing.
+def count_substring(string, sub_string):
+    return sum(1 for i in range(len(string) - len(sub_string) + 1) if string.startswith(sub_string, i))
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    count = count_substring(string, sub_string)
+    print(count)
