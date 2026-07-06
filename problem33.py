73bd86ac1f73f52c9fecbe3b4f7650a7b7312ed9
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-07-06 23:42:52 +0530

    implement dynamic ascii door mat using optimal string justification and symmetry

---
+++ problem33.py
@@ -0,0 +1,7 @@
+# HackerRank: Generate an aligned, symmetrical ASCII door mat using string centering and list reversals.
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    
+    pattern = [('.|.' * (2 * i + 1)).center(m, '-') for i in range(n // 2)]
+    
+    print('\n'.join(pattern + ['WELCOME'.center(m, '-')] + pattern[::-1]))
