10f007ba5a5f5bbdac51d07b4443c5bba6ee8986
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-07-07 20:12:23 +0530

    feat : solve day of week calculation using optimal calendar module

---
+++ problem40.py
@@ -0,0 +1,5 @@
+# HackerRank: Determine the day of the week for a given date using the standard calendar module.
+import calendar
+if __name__ == '__main__':
+    month, day, year = map(int, input().split())
+    print(calendar.day_name[calendar.weekday(year, month, day)].upper())
