7bc297a5534c7acd64a8339e7159f3c6fa87e127
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-07-15 20:57:19 +0530

    implement difference class with min max calculation

--- problem48.py
+++ problem48.py
@@ -1,1 +1,26 @@
-problem48.py
+#14/07/2026
+#30 DAYS OF CODE IN PYTHON [DAY 13]
+#ABSTRACT CLASSES 
+# HackerRank: Implement MyBook class inheriting from Book with no comments
+from abc import ABCMeta, abstractmethod
+class Book(object, metaclass=ABCMeta):
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author   
+    @abstractmethod
+    def display(): pass
+class MyBook(Book):
+    def __init__(self, title, author, price):
+        super().__init__(title, author)
+        self.price = price
+
+    def display(self):
+        print("Title: " + self.title)
+        print("Author: " + self.author)
+        print("Price: " + str(self.price))#Write MyBook class
+
+title=input()
+author=input()
+price=int(input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
