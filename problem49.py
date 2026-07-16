0abb6293ec6a2197bbe038531b04f66b11e5d6f4
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-07-16 21:25:36 +0530

    implement linked list tail insertion with standard while loop

--- problem49.py
+++ problem49.py
@@ -1,1 +1,21 @@
-problem49.py
+#15/07/2026
+#30 DAYS OF CODE IN PYTHON [DAY 14]
+#SCOPE
+# HackerRank: Calculate the maximum absolute difference using class scope.
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+        self.maximumDifference = 0
+
+    def computeDifference(self):
+        min_element = min(self.__elements)
+        max_element = max(self.__elements)
+        self.maximumDifference = max_element - min_element
+
+_ = input()
+a = [int(e) for e in input().split(' ')]
+
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)  
---
+++ problem50.py
@@ -0,0 +1,35 @@
+#16/07/2026
+#30 DAY OF CODE IN PYTHON [DAY 15]
+#LINKED LIST 
+# HackerRank: Insert a node at the tail of a linked list using basic traversal.
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data):
+        new_node = Node(data)
+        
+        if head is None:
+            return new_node
+            
+        current = head
+        while current.next is not None:
+            current = current.next
+            
+        current.next = new_node
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head); 	  
