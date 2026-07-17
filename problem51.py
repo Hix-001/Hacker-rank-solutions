e1cffa4c6382978ae4e676fd4206d9735ebd401d
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-07-17 20:30:34 +0530

    strict exception handling for string to integer conversion

---
+++ problem51.py
@@ -0,0 +1,9 @@
+#17/07/2026
+#30 DAYS OF CODE IN PYTHON [DAY 16]
+#EXCEPTIONS - STRINGS TO INTEGER 
+# HackerRank: Parse an integer from a string and handle errors using try/except blocks.
+
+try:
+    print(int(input()))
+except ValueError:
+    print("Bad String")
