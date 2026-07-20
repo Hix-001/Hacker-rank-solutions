e723995f0e6a7c072d1265c925adbf3fee2f2c28
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-07-20 20:05:58 +0530

    fix

--- problem53.py
+++ problem53.py
@@ -1,1 +1,48 @@
-problem53.py
+#19/07/2026
+#30 DAYS OF CODE IN PYTHON [DAY 18]
+#QUERIES AND STACKS
+# HackerRank: Determine if a string is a palindrome using Stacks and Queues.
+import sys
+
+class Solution:
+    def __init__(self):
+        self.stack = []
+        self.queue = []
+
+    def pushCharacter(self, ch):
+        self.stack.append(ch)
+
+    def enqueueCharacter(self, ch):
+        self.queue.append(ch)
+
+    def popCharacter(self):
+        return self.stack.pop()
+
+    def dequeueCharacter(self):
+        return self.queue.pop(0)
+# read the string s
+s=input()
+#Create the Solution class object
+obj=Solution()   
+
+l=len(s)
+# push/enqueue all the characters of string s to stack
+for i in range(l):
+    obj.pushCharacter(s[i])
+    obj.enqueueCharacter(s[i])
+    
+isPalindrome=True
+'''
+pop the top character from stack
+dequeue the first character from queue
+compare both the characters
+''' 
+for i in range(l // 2):
+    if obj.popCharacter()!=obj.dequeueCharacter():
+        isPalindrome=False
+        break
+#finally print whether string s is palindrome or not.
+if isPalindrome:
+    print("The word, "+s+", is a palindrome.")
+else:
+    print("The word, "+s+", is not a palindrome.")    
--- problem55.py
+++ problem55.py
@@ -1,1 +1,18 @@
-problem55.py
+#19/07/2026
+#Medium
+#MERGE THE TOOLS
+# HackerRank: Merge the Tools! using basic string slicing and iteration.
+
+def merge_the_tools(string, k):
+    for i in range(0, len(string), k):
+        chunk = string[i:i+k]
+        result = ""
+        
+        for char in chunk:
+            if char not in result:
+                result += char
+                
+        print(result)
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
