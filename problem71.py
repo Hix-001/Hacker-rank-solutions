dbd0fe3c74729427230ca0ea3042af0a7a3ac7f5
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-07-29 16:29:07 +0530

    Filter and sort database records using Regex

---
+++ problem71.py
@@ -0,0 +1,18 @@
+#29/07/2026
+#Medium
+#30 DAY OF CODE IN PYTHON [DAY 28]
+#RegEx, Patterns, and Intro to Databases
+# HackerRank: Filter and sort a list of first names based on a regex match for a Gmail domain.
+
+import re
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    gmail_users = []
+    for _ in range(N):
+        firstName, emailID = input().rstrip().split()
+        if re.search(r'@gmail\.com$', emailID):
+            gmail_users.append(firstName)
+    gmail_users.sort()
+    for name in gmail_users:
+        print(name)
