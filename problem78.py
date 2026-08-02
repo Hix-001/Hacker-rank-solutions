f3b393608c74b87b2a789e0f2044854c2214f51e
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-02 18:04:43 +0530

    Solve ginortS sorting challenge using advanced lambda tuple key

---
+++ problem78.py
@@ -0,0 +1,8 @@
+#02/08/2026
+#Medium
+#ginortS
+# HackerRank: Sort a string prioritizing lowercase, then uppercase, then odd digits, then even digits.
+if __name__ == '__main__':
+    s = input()
+    sorted_s = sorted(s, key=lambda c: (c.isdigit(), c in '02468', c.isupper(), c))
+    print("".join(sorted_s))
