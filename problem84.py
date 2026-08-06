22113c0eeb0f4a11b9481476017b4debcbe815b7
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-06 21:45:07 +0530

    Fix non-ASCII character error in Find Angle MBC by using chr(176)

--- problem84.py
+++ problem84.py
@@ -3,8 +3,9 @@
 #Find Angle MBC
 # HackerRank: Calculate the angle of a right triangle's median using trigonometry and math.atan2.
 import math
+
 ab = int(input())
 bc = int(input())
 angle_rad = math.atan2(ab, bc)
 angle_deg = round(math.degrees(angle_rad))
-print(f"{angle_deg}°")
+print(f"{angle_deg}{chr(176)}")
