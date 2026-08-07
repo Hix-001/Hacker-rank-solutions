bfbcaf485296d83f5576b4b95a4d4f447e098755
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-08 00:09:23 +0530

    Solve No Idea! using O(1) set lookups and boolean arithmetic

---
+++ problem85.py
@@ -0,0 +1,10 @@
+#08/08/2026
+#Medium
+#No Idea!
+# HackerRank: Calculate a score by checking array elements against positive and negative hash sets.
+n, m = map(int, input().split())
+arr = input().split()
+A = set(input().split())
+B = set(input().split())
+happiness = sum((i in A) - (i in B) for i in arr)
+print(happiness)
