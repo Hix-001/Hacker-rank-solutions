707fde13a79b9372b441eda8e2785b942efa2087
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-10 17:40:03 +0530

    Solve Input() polynomial challenge using Python's eval function

--- problem88.py
+++ problem88.py
@@ -1,1 +1,10 @@
-problem88.py
+#09/08/2026
+#Easy
+#itertools.combinations_with_replacement()
+# HackerRank: Generate all combinations of a string of size k allowing character replacement.
+from itertools import combinations_with_replacement
+s, k = input().split()
+k = int(k)
+sorted_s = sorted(s)
+for c in combinations_with_replacement(sorted_s, k):
+    print("".join(c))
---
+++ problem90.py
@@ -0,0 +1,8 @@
+#10/08/2026
+#Easy
+#Input()
+# HackerRank: Evaluate a polynomial string mathematically using the eval() function.
+if __name__ == '__main__':
+    x, k = map(int, input().split())
+    poly = input()
+    print(eval(poly) == k)
