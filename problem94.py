9c58be755ea616a76fe7c2955329260557412adb
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-14 23:18:13 +0530

    Fix IndentationError in Set Mutations logic

--- problem94.py
+++ problem94.py
@@ -1,1 +1,15 @@
-problem94.py
+#14/08/2026
+#Medium
+#Set Mutations
+# HackerRank: Execute dynamic in-place set mutation methods and calculate the final sum.
+
+_ = int(input())
+A = set(map(int, input().split()))
+n = int(input())
+
+for _ in range(n):
+    command, _ = input().split()
+    other_set = set(map(int, input().split()))
+    getattr(A, command)(other_set)
+
+print(sum(A))
