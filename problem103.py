cba5b71fd7b7d3526bf44d67bcfd61df3d1f1343
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-22 22:10:15 +0530

    HTML Parser - Part 1 HackerRank built-ins challenge

---
+++ problem103.py
@@ -0,0 +1,25 @@
+#22/08/2026
+#Easy
+#HTML Parser - Part 1
+# HackerRank: Subclass HTMLParser to extract tags and attributes.
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print(f"Start : {tag}")
+        for name, value in attrs:
+            print(f"-> {name} > {value}")
+
+    def handle_endtag(self, tag):
+        print(f"End   : {tag}")
+
+    def handle_startendtag(self, tag, attrs):
+        print(f"Empty : {tag}")
+        for name, value in attrs:
+            print(f"-> {name} > {value}")
+
+parser = MyHTMLParser()
+n = int(input())
+
+for _ in range(n):
+    parser.feed(input())
