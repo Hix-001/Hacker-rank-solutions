9492aa6816dd5c206a126891e3bd258138313c0a
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-22 22:13:17 +0530

    HTML Parser - Part 2 HackerRank challenge

---
+++ problem104.py
@@ -0,0 +1,29 @@
+#22/08/2026
+#Easy
+#HTML Parser - Part 2
+# HackerRank: Extract and format HTML comments and data streams.
+
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_comment(self, data):
+        if '\n' in data:
+            print(">>> Multi-line Comment")
+            print(data)
+        else:
+            print(">>> Single-line Comment")
+            print(data)
+
+    def handle_data(self, data):
+        if data != '\n':
+            print(">>> Data")
+            print(data)
+
+html = ""       
+for i in range(int(input())):
+    html += input().rstrip()
+    html += '\n'
+    
+parser = MyHTMLParser()
+parser.feed(html)
+parser.close()
