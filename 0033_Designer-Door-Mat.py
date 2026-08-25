bf7eb10b4d4ab0ff6135e449bd04f5feea9381d5
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-25 14:26:16 +0530

    Update metadata: 0033_Designer-Door-Mat.py

--- 0033_Designer-Door-Mat.py
+++ 0033_Designer-Door-Mat.py
@@ -1,4 +1,8 @@
+#06/07/2026
+#Easy
+#Designer Door Mat
 # HackerRank: Generate an aligned, symmetrical ASCII door mat using string centering and list reversals.
+
 if __name__ == '__main__':
     n, m = map(int, input().split())
     
