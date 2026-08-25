0a32c2db342dfe6c2536936b0343077894a17189
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-25 14:26:10 +0530

    Update metadata: 0022_sWAP-cASE.py

--- 0022_sWAP-cASE.py
+++ 0022_sWAP-cASE.py
@@ -1,4 +1,8 @@
-# HackerRank: Given a string, return a new string where all lowercase letters are converted to uppercase letters and vice versa.
+#05/07/2026
+#Easy
+#sWAP cASE
+# HackerRank: Given a string, return a new string where all lowercase letters are converted to uppercase and vice versa.
+
 def swap_case(s):
     return s.swapcase()
 if __name__ == '__main__':
