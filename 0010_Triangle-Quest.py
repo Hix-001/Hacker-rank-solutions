d9895d2c1b726032eedeb2efb67bb974b618a0d7
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-25 14:26:03 +0530

    Update metadata: 0010_Triangle-Quest.py

--- 0010_Triangle-Quest.py
+++ 0010_Triangle-Quest.py
@@ -1,4 +1,8 @@
+#05/07/2026
+#Medium
+#Triangle Quest
 # HackerRank: Print a numerical triangle of height N-1 where each row i contains the digit i repeated i times using only arithmetic.
+
 for i in range(1, int(input())):
     print(i * (10**i - 1) // 9)
     
