1b37b2310a7a419212498cd9596b5665fc403b99
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-25 14:26:55 +0530

    Update metadata: 0078_ginortS.py

--- 0078_ginortS.py
+++ 0078_ginortS.py
@@ -2,6 +2,7 @@
 #Medium
 #ginortS
 # HackerRank: Sort a string prioritizing lowercase, then uppercase, then odd digits, then even digits.
+
 if __name__ == '__main__':
     s = input()
     sorted_s = sorted(s, key=lambda c: (c.isdigit(), c in '02468', c.isupper(), c))
