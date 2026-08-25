637b28a5b548db43d98a0340ec6bc3ec8f4aa5df
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-25 14:26:10 +0530

    Update metadata: 0021_Day-02-Operators.py

--- 0021_Day-02-Operators.py
+++ 0021_Day-02-Operators.py
@@ -1,5 +1,8 @@
-#30 DAYS OF CODE IN PYTHON [DAY 02]
-# HackerRank: Calculate and print the total cost of a meal (rounded to the nearest integer) given its base cost, tip percentage, and tax percentage.
+#05/07/2026
+#Easy
+#Operators
+# HackerRank: Calculate and print the total cost of a meal (rounded to the nearest integer) given base cost, tip percent, and tax percent.
+
 def solve(meal_cost, tip_percent, tax_percent):
     tip = meal_cost * (tip_percent / 100)
     tax = meal_cost * (tax_percent / 100)
