4ae663b53e66035fef21b728d33c02bb594b35fd
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-25 14:26:19 +0530

    Update metadata: 0038_Day-05-Loops.py

--- 0038_Day-05-Loops.py
+++ 0038_Day-05-Loops.py
@@ -1,5 +1,8 @@
-#30 DAY OF CODE IN PYTHON [DAY 05]
+#08/07/2026
+#Easy
+#Loops
 # HackerRank: Print the first 10 multiples of an integer using an iterative loop and f-string formatting.
+
 if __name__ == '__main__':
     n = int(input().strip())
     
