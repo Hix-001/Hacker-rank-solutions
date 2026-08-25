01a53903f32779906cc4c5d36b142658f4904013
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-25 14:25:59 +0530

    Update metadata: 0005_Write-a-function.py

--- 0005_Write-a-function.py
+++ 0005_Write-a-function.py
@@ -1,4 +1,8 @@
-# HackerRank: Given a year as an integer, determine if it is a leap year by evaluating if it is evenly divisible by 4, excluding century years unless they are also divisible by 400.
+#05/07/2026
+#Medium
+#Write a function
+# HackerRank: Given a year as an integer, determine if it is a leap year by evaluating if it is evenly divisible by 4, excluding century years unless divisible by 400.
+
 def is_leap(year):
     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 year = int(input("Enter a year: "))
