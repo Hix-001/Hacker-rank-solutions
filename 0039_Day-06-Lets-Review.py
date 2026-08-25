337588d158da40e7aabff377b89cc9d6bd78a260
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-25 14:26:20 +0530

    Update metadata: 0039_Day-06-Lets-Review.py

--- 0039_Day-06-Lets-Review.py
+++ 0039_Day-06-Lets-Review.py
@@ -1,5 +1,8 @@
-#30 DAYS OF CODE IN PYTHON [DAY 06]
+#07/07/2026
+#Easy
+#Let's Review
 # HackerRank: Separate a string into even and odd indexed characters using optimal string slicing.
+
 if __name__ == '__main__':
     T = int(input().strip())
     for _ in range(T):
