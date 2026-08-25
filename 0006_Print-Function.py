c5924a96548d1fc1ae07c89dc1590adfd15e481d
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-25 14:26:00 +0530

    Update metadata: 0006_Print-Function.py

--- 0006_Print-Function.py
+++ 0006_Print-Function.py
@@ -1,4 +1,8 @@
+#05/07/2026
+#Easy
+#Print Function
 # HackerRank: Print the sequence of numbers from 1 to n concatenated without spaces.
+
 if __name__ == '__main__':
     n = int(input())
     for i in range(1, n + 1):
