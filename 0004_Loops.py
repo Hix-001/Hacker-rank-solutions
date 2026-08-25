7860a8d1dc745c37ab30a16ba02fdea89a29c3c8
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-25 14:25:58 +0530

    Update metadata: 0004_Loops.py

--- 0004_Loops.py
+++ 0004_Loops.py
@@ -1,4 +1,8 @@
+#05/07/2026
+#Easy
+#Loops
 # HackerRank: Given an integer n, print the square of every non-negative integer i < n.
+
 if __name__ == '__main__':
     n = int(input())
     for i in range(n):
