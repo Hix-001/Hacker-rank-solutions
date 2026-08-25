0770b29ea50ea1229147fca25f0b44861119b467
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-25 14:26:02 +0530

    Update metadata: 0009_Integers-In-All-Sizes.py

--- 0009_Integers-In-All-Sizes.py
+++ 0009_Integers-In-All-Sizes.py
@@ -1,4 +1,8 @@
+#05/07/2026
+#Easy
+#Integers In All Sizes
 # HackerRank: Read four integers a, b, c, d and print a^b + c^d.
+
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
