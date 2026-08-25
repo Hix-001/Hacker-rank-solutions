0e25b302a2d4a51009dc5b02b9cf6845f223dace
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-25 14:26:11 +0530

    Update metadata: 0024_Whats-Your-Name.py

--- 0024_Whats-Your-Name.py
+++ 0024_Whats-Your-Name.py
@@ -1,4 +1,8 @@
+#05/07/2026
+#Easy
+#What's Your Name?
 # HackerRank: Given a first and last name, efficiently inject them into a specific greeting string and print the result.
+
 def print_full_name(first, last):
     print(f"Hello {first} {last}! You just delved into python.")
 if __name__ == '__main__':
