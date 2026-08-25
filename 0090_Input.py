cd74d3e084587da039de5ec01ab7317d9030e97a
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-25 14:27:01 +0530

    Update metadata: 0090_Input.py

--- 0090_Input.py
+++ 0090_Input.py
@@ -2,6 +2,7 @@
 #Easy
 #Input()
 # HackerRank: Evaluate a polynomial string mathematically using the eval() function.
+
 if __name__ == '__main__':
     x, k = map(int, input().split())
     poly = input()
