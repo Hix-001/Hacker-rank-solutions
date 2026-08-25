11c2db181790fcca4963def0b0a053d2e294d8c5
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-25 14:26:59 +0530

    Update metadata: 0085_No-Idea.py

--- 0085_No-Idea.py
+++ 0085_No-Idea.py
@@ -2,6 +2,7 @@
 #Medium
 #No Idea!
 # HackerRank: Calculate a score by checking array elements against positive and negative hash sets.
+
 n, m = map(int, input().split())
 arr = input().split()
 A = set(input().split())
