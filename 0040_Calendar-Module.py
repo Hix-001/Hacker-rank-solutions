68ecdaae8b96398e5361d5b4549108a7dfaf6d84
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-25 14:26:21 +0530

    Update metadata: 0040_Calendar-Module.py

--- 0040_Calendar-Module.py
+++ 0040_Calendar-Module.py
@@ -1,4 +1,8 @@
+#07/07/2026
+#Easy
+#Calendar Module
 # HackerRank: Determine the day of the week for a given date using the standard calendar module.
+
 import calendar
 if __name__ == '__main__':
     month, day, year = map(int, input().split())
