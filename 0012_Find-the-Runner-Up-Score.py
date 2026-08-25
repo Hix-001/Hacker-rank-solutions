a0e87d5f00b9c9cf9a23b074f9ac9c25c3ff5d13
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-25 14:26:04 +0530

    Update metadata: 0012_Find-the-Runner-Up-Score.py

--- 0012_Find-the-Runner-Up-Score.py
+++ 0012_Find-the-Runner-Up-Score.py
@@ -1,4 +1,8 @@
-# HackerRank: Runner-Up Score: Given a list of participant scores, identify the second-highest unique value.
+#05/07/2026
+#Easy
+#Find the Runner-Up Score!
+# HackerRank: Given a list of participant scores, identify the second-highest unique value.
+
 if __name__ == '__main__':
     n = int(input())
     arr = list(set(map(int, input().split())))
