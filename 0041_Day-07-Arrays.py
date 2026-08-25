436649280afbaba0f679ed6fee8d2ec3fa5fa60a
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-25 14:26:22 +0530

    Update metadata: 0041_Day-07-Arrays.py

--- 0041_Day-07-Arrays.py
+++ 0041_Day-07-Arrays.py
@@ -1,7 +1,8 @@
 #08/07/2026
-#30 DAYS OF CODE IN PYTHON [DAY 07]
-#ARRAYS
+#Easy
+#Arrays
 # HackerRank: Reverse an array and print space-separated elements using optimal slicing and unpacking.
+
 if __name__ == '__main__':
     n = int(input().strip())
     arr = input().strip().split()
