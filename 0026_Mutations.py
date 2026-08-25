c27f9e71106856d6911c95f7d5c0666f3234869d
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-25 14:26:13 +0530

    Update metadata: 0026_Mutations.py

--- 0026_Mutations.py
+++ 0026_Mutations.py
@@ -1,4 +1,8 @@
+#04/07/2026
+#Easy
+#Mutations
 # HackerRank: Replace a character at a specific index in an immutable string using optimal slicing.
+
 def count_substring(string, sub_string):
     return sum(1 for i in range(len(string) - len(sub_string) + 1) if string.startswith(sub_string, i))
 if __name__ == '__main__':
