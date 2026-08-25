d6ff8d627a0c8d8b6691b8c43a8e168a827a701c
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-25 14:26:52 +0530

    Update metadata: 0073_Collections-namedtuple.py

--- 0073_Collections-namedtuple.py
+++ 0073_Collections-namedtuple.py
@@ -2,6 +2,7 @@
 #Easy
 #Collections.namedtuple()
 # HackerRank: Use namedtuple to extract student marks from columns in an unknown order and compute the average.
+
 from collections import namedtuple
 n = int(input())
 Student = namedtuple('Student', input().split())
