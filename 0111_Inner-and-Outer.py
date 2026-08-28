9d2516b9ba389cd675aad7c265baad37388a2fc9
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-28 19:56:16 +0530

    Inner and Outer HackerRank numpy challenge

---
+++ 0111_Inner-and-Outer.py
@@ -0,0 +1,12 @@
+#28/08/2026
+#Easy
+#Inner and Outer
+# HackerRank: Compute mathematical inner and outer products of 1-D vectors using Numpy.
+
+import numpy
+
+a = numpy.array(input().split(), int)
+b = numpy.array(input().split(), int)
+
+print(numpy.inner(a, b))
+print(numpy.outer(a, b))
