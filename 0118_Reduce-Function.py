33cdb2c17b21f79c879bf7e02522618c9eb93f5c
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-09-03 16:56:25 +0530

    Reduce Function HackerRank functools challenge

---
+++ 0118_Reduce-Function.py
@@ -0,0 +1,18 @@
+#03/09/2026
+#Medium
+#Reduce Function
+# HackerRank: Multiply a list of rational numbers using functools.reduce.
+
+from fractions import Fraction
+from functools import reduce
+
+def product(fracs):
+    t = reduce(lambda x, y: x * y, fracs)
+    return t.numerator, t.denominator
+
+if __name__ == '__main__':
+    fracs = []
+    for _ in range(int(input())):
+        fracs.append(Fraction(*map(int, input().split())))
+    result = product(fracs)
+    print(*result)
