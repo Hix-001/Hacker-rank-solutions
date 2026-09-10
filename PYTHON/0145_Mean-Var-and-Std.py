3ff35031f2d1db32de531b8295269f0d831085b3
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-09-10 16:49:28 +0530

    Mean Var and Std HackerRank numpy statistics challenge

---
+++ PYTHON/0145_Mean-Var-and-Std.py
@@ -0,0 +1,11 @@
+#10/09/2026
+#Easy
+#Mean, Var, and Std
+# HackerRank: Compute basic statistical measures along specific array axes using NumPy.
+
+import numpy
+n, m = map(int, input().split())
+arr = numpy.array([input().split() for _ in range(n)], int)
+print(numpy.mean(arr, axis=1))
+print(numpy.var(arr, axis=0))
+print(round(numpy.std(arr, axis=None), 11))
