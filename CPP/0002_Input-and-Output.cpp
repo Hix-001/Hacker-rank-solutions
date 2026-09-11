e342bf240072fc312823b441980d4fd98af9d3d7
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-09-11 12:26:24 +0530

    Input and Output HackerRank C-style I/O challenge

---
+++ CPP/0002_Input-and-Output.cpp
@@ -0,0 +1,13 @@
+// 11/09/2026
+// Easy
+// Input and Output
+// HackerRank: Read variables using scanf and print their sum using printf.
+
+#include <cstdio>
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    int sum = a + b + c;
+    printf("%d\n", sum);
+    return 0;
+}
