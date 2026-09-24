349c47daa76ab28501faf5ff74a9cfddf2b80828
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-09-24 14:00:59 +0530

    Pointer HackerRank passing by reference challenge

--- CPP/0015_Pointer.cpp
+++ CPP/0015_Pointer.cpp
@@ -1,1 +1,19 @@
-0015_Pointer.cpp
+// 24/09/2026
+// Easy
+// Pointer
+// HackerRank: Modify variables in place by passing memory addresses to a function.
+#include <cstdio>
+#include <cmath>
+void update(int *a, int *b) {
+    int sum = *a + *b;
+    int diff = std::abs(*a - *b);
+    *a = sum;
+    *b = diff;
+}
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    update(&a, &b);
+    printf("%d\n%d\n", a, b);
+    return 0;
+}
